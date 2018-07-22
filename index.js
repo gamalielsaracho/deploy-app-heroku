e236c653ee2f02dfba082364e0d825ab853e08b3
gamaliel <fabiogamaliel@gmail.com>
2018-07-21 23:37:41 -0400

    port variable

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
-
-var config = require('./config')
+var PORT = process.env.PORT || 5000
+// var config = require('./config')
 
 // static files.
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
-	res.sendfile('./public/index.html')
+	res.sendfile('index.html')
 })
 
 
-app.listen(config.server.port, function () {
-	
-	console.log('Corriendo en el puerto'+config.server.port)
+app.listen(PORT, function (err) {
+	if(err) {
+		console.log('Error al correr en el puerto '+PORT)
+	}
+
+	console.log('Corriendo en el puerto '+PORT)
 })
